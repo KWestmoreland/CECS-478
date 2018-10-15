f1293af66b63be1eaa2ef99aac23ede8977fbab7
Luis Bri <lbp38@alu.ua.es>
2018-10-15 05:25:20 +0200

    Encryption code with JSON output

--- StandAloneApp.py
+++ StandAloneApp.py
@@ -6,6 +6,7 @@ Created on Wed Oct 10 00:34:08 2018
 """
     
 import os
+import json
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
@@ -31,36 +32,10 @@ def load_key(filename):
         pemlines = pem_in.read()
     public_key = load_pem_public_key(pemlines, default_backend())
     return public_key
-    
-if __name__ == '__main__':
-    public_key_hold = load_key(b"C:\Users\Luisa\Documents\RSA_public.pem")
-    #print(public_key_hold.key_size)
-    
-    backend = default_backend()
-    key = os.urandom(32)
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
-    ct = encrypt(cipher, b"Hello world!!!!!")
-    #print(ct)
-    pt = decrypt(cipher, ct)
-    #print(pt)
-    
-    key2 = os.urandom(32)
-    h = hmac.HMAC(key2, hashes.SHA256(), backend=default_backend())
-    h.update(ct)
-    signature = h.finalize()
-    #print(signature)
-    
-    
-    h2 = hmac.HMAC(key2, hashes.SHA256(), backend=default_backend())
-    h2.update(ct)
-    #print(h2.verify(signature))
-    
-    concatenated = key + key2
-    #print(concatenated)
-    
-    cipherKeys = public_key_hold.encrypt(
-            concatenated, 
+
+def rsaEncrypt(plaintext, publicKey):
+    ciphertext = publicKey.encrypt(
+            plaintext, 
             padding.OAEP(
                     mgf=padding.MGF1(algorithm=hashes.SHA256()),
                     algorithm=hashes.SHA256(),
@@ -68,9 +43,75 @@ if __name__ == '__main__':
             )            
     )
             
-    print("RSA ciphertext: {}" .format(cipherKeys))
+    return ciphertext
+
+def sign(messageToSign):
+    h.update(messageToSign)
+    signature = h.finalize()
+    
+    return signature
+
+class Json(object):
+        rsa = ""
+        aes = ""
+        hmac = ""
+    
+        # The class "constructor" - It's actually an initializer 
+        def __init__(self, rsa, aes, hmac):
+            self.rsa = rsa
+            self.aes = aes
+            self.hmac = hmac
+
+def make_Json(rsa, aes, hmac):
+    json = Json(rsa, aes, hmac)
+    return json    
+    
+if __name__ == '__main__':
+    #Setup encryption variables
+    public_key_hold = load_key(b"C:\Users\Luisa\Documents\RSA_public.pem")    
+    backend = default_backend()
+    key = os.urandom(32)
+    iv = os.urandom(16)   
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=backend)
+    
+    #Encrypt text
+    ct = encrypt(cipher, b"Hello world!!!!!")
+
+    #Decrypt text
+    pt = decrypt(cipher, ct)
+    
+    #Setup signature
+    key2 = os.urandom(32)
+    h = hmac.HMAC(key2, hashes.SHA256(), backend=default_backend())
+    
+    #Sign ciphertext
+    signature = sign(ct)
+    
+    #Setup verification signature (only for text purposes)
+    h2 = hmac.HMAC(key2, hashes.SHA256(), backend=default_backend())
+    h2.update(ct)
+    
+    #Verify signature (no message == OK)
+    h2.verify(signature)
+    
+    #Concatenate keys to send to receiver
+    concatenated = key + key2
+
+    #Encrypt concatenated keys with RSA    
+    cipherKeys = rsaEncrypt(concatenated, public_key_hold)
+    
+    #Print in JSON (problems while decoding bytes)
+    print(json.dumps(
+            {'RSA ciphertext': cipherKeys.decode('utf-8', 'replace'), 'AES ciphertext': ct.decode('utf-8', 'replace'), 'HMAC tag': signature.decode('utf-8', 'replace')},
+            sort_keys=True,
+            indent=4, 
+            separators=(',', ': ')
+        )
+    )
+            
+    '''print("RSA ciphertext: {}" .format(cipherKeys))
     print("AES ciphertext: {}" .format(ct))
-    print("HMAC tag: {}" .format(signature))
+    print("HMAC tag: {}" .format(signature))'''
 
 
     
